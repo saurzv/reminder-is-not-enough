9b44d17c1937740935336ceb271d932db5cd13d1
Saurav Raj <saurav.raj5592+git@gmail.com>
2023-03-10 18:20:10 +0530

    Modify templates

--- rine/main/routes.py
+++ rine/main/routes.py
@@ -17,12 +17,15 @@ def index():
             datetime.today().strftime("%Y-%m-%d")), sender=os.environ.get('DMAIL'), recipients=[os.environ.get('SEND_TO')])
         curr_time = datetime.today()
         to_send = []
+        k = 0
         for obj in email_db.find():
             deadl = obj['deadl']
             day_diff = (deadl - curr_time).days
             if (day_diff < 0):
                 email_db.find_one_and_delete({'_id': obj['_id']})
             elif (day_diff <= 30):
+                k += 1
+                obj['n'] = k
                 to_send.append(obj)
 
         if (len(to_send) > 0):
